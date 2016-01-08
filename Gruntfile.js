a98985cc80a4024d2687b08d26fa329001c7c4f8
Nick Niles <nickyniles@gmail.com>
2016-01-08 23:49:13 +0100

    basic setup finished

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,121 +1,275 @@
-/*
- * Generated on 2016-01-08
- * generator-assemble v0.5.0
- * https://github.com/assemble/generator-assemble
- *
- * Copyright (c) 2016 Hariadi Hinta
- * Licensed under the MIT license.
- */
-
 'use strict';
-
-// # Globbing
-// for performance reasons we're only matching one level down:
-// '<%= config.src %>/templates/pages/{,*/}*.hbs'
-// use this if you want to match all subfolders:
-// '<%= config.src %>/templates/pages/**/*.hbs'
 
 module.exports = function(grunt) {
 
-  require('time-grunt')(grunt);
-  require('load-grunt-tasks')(grunt);
+    require('load-grunt-tasks')(grunt);
 
-  // Project configuration.
-  grunt.initConfig({
 
-    config: {
-      src: 'src',
-      dist: 'dist'
-    },
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        app: 'src',
+        dist: 'dist',
 
-    watch: {
-      assemble: {
-        files: ['<%= config.src %>/{content,data,templates}/{,*/}*.{md,hbs,yml}'],
-        tasks: ['assemble']
-      },
-      livereload: {
-        options: {
-          livereload: '<%= connect.options.livereload %>'
+
+
+        convert: {
+            options: {
+                explicitArray: false,
+            },
+            csv2json: {
+                src: ['<%= app %>/data/publishers.csv'],
+                dest: '<%= app %>/data/publishers.json'
+            }
         },
-        files: [
-          '<%= config.dist %>/{,*/}*.html',
-          '<%= config.dist %>/assets/{,*/}*.css',
-          '<%= config.dist %>/assets/{,*/}*.js',
-          '<%= config.dist %>/assets/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
-        ]
-      }
-    },
+        // pageres: {
+        //     screenshot: {
+        //         options: {
+        //             urls: ['127.0.0.1:9000', '127.0.0.1:9000/explore.html', '127.0.0.1:9000/results.html'],
+       
+        // }, //             sizes: ['1200x800'],
+        //             dest: '<%= app %>/img/screenshots',
+        //             crop: true
+        //         }
+        //     }
 
-    connect: {
-      options: {
-        port: 9000,
-        livereload: 35729,
-        // change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost'
-      },
-      livereload: {
-        options: {
-          open: true,
-          base: [
-            '<%= config.dist %>'
-          ]
-        }
-      }
-    },
 
-    assemble: {
-      pages: {
-        options: {
-          flatten: true,
-          assets: '<%= config.dist %>/assets',
-          layout: '<%= config.src %>/templates/layouts/default.hbs',
-          data: '<%= config.src %>/data/*.{json,yml}',
-          partials: '<%= config.src %>/templates/partials/*.hbs',
-          plugins: ['assemble-contrib-permalinks','assemble-contrib-sitemap'],
+        sass: {
+            options: {
+
+                includePaths: ['<%= app %>/bower_components/foundation-sites/scss']
+            },
+            dist: {
+                options: {
+                    outputStyle: 'extended'
+                },
+                files: {
+                    '<%= app %>/css/app.css': '<%= app %>/scss/app.scss'
+                }
+            }
         },
-        files: {
-          '<%= config.dist %>/': ['<%= config.src %>/templates/pages/*.hbs']
+
+        assemble: {
+            pages: {
+                options: {
+                    flatten: true,
+                    assets: '<%= app %>',
+                    layout: '<%= app %>/templates/layouts/default.hbs',
+                    data: '<%= app %>/data/*.{json,yml}',
+                    partials: '<%= app %>/templates/partials/*.hbs',
+                    plugins: ['assemble-contrib-permalinks', 'assemble-contrib-sitemap'],
+                },
+                files: {
+                    '<%= app %>/': ['<%= app %>/templates/pages/*.hbs']
+                }
+            }
+        },
+
+        browserSync: {
+            options: {
+                notify: false,
+                background: true
+            },
+            livereload: {
+                options: {
+                    files: [
+                        '<%= app %>/{,*/}*.html',
+                        '.tmp/styles/{,*/}*.css',
+                        '<%= app %>/images/{,*/}*',
+                        '.tmp/scripts/{,*/}*.js'
+                    ],
+                    port: 9003,
+                    server: {
+                        baseDir: ['.tmp', 'app'],
+                        routes: {
+                            '/bower_components': './bower_components'
+                        }
+                    }
+                }
+            },
+            // test: {
+            //   options: {
+            //     port: 9001,
+            //     open: false,
+            //     logLevel: 'silent',
+            //     host: 'localhost',
+            //     server: {
+            //       baseDir: ['.tmp', './test', config.app],
+            //       routes: {
+            //         '/bower_components': './bower_components'
+            //       }
+            //     }
+            //   }
+            // },
+            dist: {
+                options: {
+                    background: false,
+                    server: '<%= dist %>'
+                }
+            }
+        },
+
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: [
+                'Gruntfile.js',
+                '<%= app %>/js/**/*.js'
+            ]
+        },
+
+        clean: {
+            dist: {
+                src: ['<%= dist %>/*']
+            },
+        },
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= app %>/',
+                    src: [
+                        'fonts/**',
+                        '**/*.html',
+                        '!**/*.scss',
+                        '!bower_components/**',
+                        'js/sup-topbar.min.js'
+                    ],
+                    dest: '<%= dist %>/'
+                }]
+            },
+        },
+
+        imagemin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= app %>/img/',
+                    src: ['**/*.{jpg,gif,svg,jpeg,png}'],
+                    dest: '<%= dist %>/img/'
+                }]
+            }
+        },
+
+        uglify: {
+            options: {
+                preserveComments: 'some',
+                mangle: false
+            }
+        },
+
+        useminPrepare: {
+            html: ['<%= app %>/index.html'],
+            options: {
+                dest: '<%= dist %>'
+            }
+        },
+
+        usemin: {
+            html: ['<%= dist %>/**/*.html', '!<%= app %>/bower_components/**'],
+            css: ['<%= dist %>/css/**/*.css'],
+            options: {
+                dirs: ['<%= dist %>']
+            }
+        },
+
+        watch: {
+            assemble: {
+                files: ['<%= app %>/{data,templates}/{,*/}*.{md,hbs,yml,haml}'],
+                tasks: ['assemble']
+            },
+            grunt: {
+                files: ['Gruntfile.js'],
+                tasks: ['sass']
+            },
+            sass: {
+                files: '<%= app %>/scss/**/*.scss',
+                tasks: ['sass']
+            },
+            // postcss: {
+            //  files: '<%= app %>/scss/**/*.scss',
+            //  tasks: ['postcss']
+            // },     
+            livereload: {
+                files: ['<%= app %>/**/*.html', '!<%= app %>/bower_components/**', '<%= app %>/js/**/*.js', '<%= app %>/css/**/*.css', '<%= app %>/scss/**/*.scss', '<%= app %>/images/**/*.{jpg,gif,svg,jpeg,png}'],
+                options: {
+                    livereload: true
+                }
+            },
+        },
+
+        connect: {
+            app: {
+                options: {
+                    port: 9003,
+                    base: '<%= app %>/',
+                    open: true,
+                    livereload: true,
+                    hostname: '127.0.0.1'
+                }
+            },
+            dist: {
+                options: {
+                    port: 9001,
+                    base: '<%= dist %>/',
+                    open: true,
+                    keepalive: true,
+                    livereload: false,
+                    hostname: '127.0.0.1'
+                }
+            }
+        },
+
+        postcss: {
+            options: {
+                map: true,
+                processors: [
+                    require('autoprefixer-core')({
+                        browsers: ['last 2 versions']
+                    })
+                ]
+            },
+            dist: {
+                src: '<%= app %>/css/*.css'
+            }
+        },
+
+        wiredep: {
+            target: {
+                src: [
+                    '<%= app %>/**/*.html'
+                ],
+                exclude: [
+                    'modernizr',
+                    'jquery-placeholder',
+                    'foundation-sites'
+                ]
+            }
         }
-      }
-    },
 
-    copy: {
-      bootstrap: {
-        expand: true,
-        cwd: 'bower_components/bootstrap/dist/',
-        src: '**',
-        dest: '<%= config.dist %>/assets/'
-      },
-      theme: {
-        expand: true,
-        cwd: 'src/assets/',
-        src: '**',
-        dest: '<%= config.dist %>/assets/css/'
-      }
-    },
+    });
 
-    // Before generating any new files,
-    // remove any previously-created files.
-    clean: ['<%= config.dist %>/**/*.{html,xml}']
 
-  });
+    grunt.registerTask('compile-sass', ['sass']);
+    grunt.registerTask('bower-install', ['wiredep']);
 
-  grunt.loadNpmTasks('assemble');
+    grunt.registerTask('default', [
+       //'convert',
+        'assemble',
+        'compile-sass',
+        // 'postcss',
 
-  grunt.registerTask('server', [
-    'build',
-    'connect:livereload',
-    'watch'
-  ]);
-
-  grunt.registerTask('build', [
-    'clean',
-    'copy',
-    'assemble'
-  ]);
-
-  grunt.registerTask('default', [
-    'build'
-  ]);
+        //'bower-install',
+        //'browserSync:livereload',
+        'connect:app',
+        'watch'
+    ]);
+    grunt.registerTask('validate-js', ['jshint']);
+    grunt.registerTask('server-dist', ['connect:dist']);
+    grunt.registerTask('data', ['convert']);
+    grunt.registerTask('publish', ['compile-sass', 'clean:dist',
+        //'validate-js', 
+        'useminPrepare', 'copy:dist', 'newer:imagemin', 'concat', 'cssmin', 'uglify', 'usemin'
+    ]);
 
 };
